5bd95bf3f8d984cd9b94ee9ee8d1816e37c1598a
David Lavelle <d.lavelle7@yahoo.ie>
2016-05-23 15:34:52 +0100

    messy messy.

--- microwave.py
+++ microwave.py
@@ -2,18 +2,48 @@
 import sys
 import traceback
 from Tkinter import *
+from collections import deque
 
 
-class Application(Frame):
+class FrameComponent(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.create_widgets()
+        self.create()
         self.pack()
 
-    def create_widgets(self):
-#        # Timer / Clock
-#        # Number pad
+    def create(self):
+        pass
+
+
+class Microwave(FrameComponent):
+
+    def __init__(self, master):
+        FrameComponent.__init__(self, master)
+        self.timer = Timer(self)
+        self.number_pad = NumberPad(self)
+        self.controls = Controls(self)
+
+
+class Timer(FrameComponent):
+
+    def __init__(self, master):
+        self._timer = deque(maxlen=4)
+        self._timer.append("0")
+        FrameComponent.__init__(self, master)
+
+    def create(self):
+        self.timer = Label(self, borderwidth=10)
+        self.refresh()
+        self.timer.pack()
+
+    def refresh(self):
+        self.timer["text"] = "".join(self._timer)
+
+
+class NumberPad(FrameComponent):
+
+    def create(self):
         num = 0
         for r in range(4):
             for c in range(3):
@@ -21,28 +51,36 @@ class Application(Frame):
                 if num in (10, 12):
                     Button(self).grid_forget()
                 elif num == 11:
-                    Button(self, text="0",
+                    NumPadButton(self, text="0",
                         borderwidth=1 ).grid(row=r,column=c)
                 else:
-                    Button(self, text="{0}".format(num),
+                    NumPadButton(self, text="{0}".format(num),
                         borderwidth=1 ).grid(row=r,column=c)
 
 
+class NumPadButton(Button):
+
+    def __init__(self, *args, **kwargs):
+        Button.__init__(self, *args, **kwargs)
+        self["command"] = self.press_num
+
+    def press_num(self):
+        self.master.master.timer._timer.append(self["text"])
+        self.master.master.timer.refresh()
+
+
+class Controls(FrameComponent):
+
+    def create(self):
         # Controls
-#        self.start = Button(self)
-#        self.start["text"] = "Start"
-#        self.start["command"] = self.start_oven
-#        self.start.pack(side=LEFT)
-#
-#        self.stop = Button(self)
-#        self.stop["text"] = "Stop"
-#        self.stop["command"] = self.stop_oven
-#        self.stop.pack(side=LEFT)
-#
-#        self._open = Button(self)
-#        self._open["text"] = "Open"
-#        self._open["command"] = self.open_oven
-#        self._open.pack(side=LEFT)
+        self.start = Button(self, text="Start", command=self.start_oven)
+        self.start.pack(side=LEFT)
+
+        self.stop = Button(self, text="Stop", command=self.stop_oven)
+        self.stop.pack(side=LEFT)
+
+        self._open = Button(self, text="Open", command=self.open_oven)
+        self._open.pack(side=LEFT)
 
     def start_oven(self):
         print "Start . . ."
@@ -54,10 +92,11 @@ class Application(Frame):
         print "Open . . ."
 
 
+
 def main():
     top = Tk()
-    app = Application(master=top)
-    app.mainloop()
+    Microwave(top)
+    top.mainloop()
 
 
 if __name__ == "__main__":
